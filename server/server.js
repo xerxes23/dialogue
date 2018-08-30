f7dac4b70767a5a5073200dd92d24a387ae70013
xerxes23 <josephmendoza23@gmail.com>
2018-08-30 14:28:41 -0400

    first commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const express = require("express");
+
+const publicPath = path.join(__dirname, "../public");
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static(publicPath));
+
+app.get("/", (req, res) => {
+  res.send("index.html");
+});
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
