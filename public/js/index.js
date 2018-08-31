fad5a886f6f3b2b10d0da5a6d7a93d010c6018b2
xerxes23 <josephmendoza23@gmail.com>
2018-08-31 16:10:09 -0400

    Integrated event acknowledgements

--- public/js/index.js
+++ public/js/index.js
@@ -11,3 +11,14 @@ socket.on("disconnect", function() {
 socket.on("newMessage", function(message) {
   console.log("new message", message);
 });
+
+socket.emit(
+  "createMessage",
+  {
+    from: "Kevin",
+    text: "A million dollars"
+  },
+  function(data) {
+    console.log("Got it", data);
+  }
+);
